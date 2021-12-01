34e57b1e9f604017023d0326a8c512aa8b42687e
Vanroy Julien <julienvanroy@yahoo.fr>
2021-12-01 19:32:42 +0100

    feat: add plane burger

--- src/Experience/Scene/PartyGame/BurgerGameScene.js
+++ src/Experience/Scene/PartyGame/BurgerGameScene.js
@@ -1,20 +1,32 @@
 import FXScene from "../Core/FXScene";
+import {Mesh, MeshBasicMaterial, PlaneBufferGeometry, Vector3} from "three";
 
 export default class BurgerGameScene extends FXScene {
     constructor() {
         super()
-        console.log('yes')
+        this.positionBurger = new Vector3(0, -1, 0)
+        this._initMeshCube()
         window.addEventListener("keydown", this._listener.bind(this), true);
+    }
+
+    _initMeshCube() {
+        this.geometryCube = new PlaneBufferGeometry(0.5, 0.5);
+        this.materialCube = new MeshBasicMaterial({color: 0xffffff});
     }
 
     _listener(event) {
         if (event.defaultPrevented) return;
         switch (event.keyCode) {
             case 32:
-                console.log("Espace")
+                const mesh = new Mesh(this.geometryCube, this.materialCube)
+                mesh.position.y = this.positionBurger.y
+
+                this.positionBurger.y += 0.5
+
+                this.scene.add(mesh)
+                console.log(mesh)
                 break;
             default:
-                console.log('oui')
                 return;
         }
         event.preventDefault();
